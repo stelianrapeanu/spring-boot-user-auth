b80b95f5615ff8eded775831a44cd1e2e0f916a2
George Rapeanu <stelianrapeanu@gmail.com>
2022-01-16 13:04:45 +0200

    added scripts and set properties for h2 datasource

---
+++ user-authentication/src/main/resources/data.sql
@@ -0,0 +1,8 @@
+INSERT INTO USER (USER_ID, LAST_NAME, FIRST_NAME, USER_NAME, PASSWORD) VALUES ('9f717667-505a-4074-ba43-ceae2c0e9f4b', 'Adams', 'Roy', 'Roy', 'password');
+INSERT INTO USER (USER_ID, LAST_NAME, FIRST_NAME, USER_NAME, PASSWORD) VALUES ('3cb69467-dcaa-4d53-84d9-c0d7ba439645', 'Adams', 'Martin', 'Martin', 'password');
+INSERT INTO USER (USER_ID, LAST_NAME, FIRST_NAME, USER_NAME, PASSWORD) VALUES ('a36df0b3-da59-47e3-b110-37a5a222d46b', 'Alvarez', 'Roger', 'Roger', 'password');
+INSERT INTO USER (USER_ID, LAST_NAME, FIRST_NAME, USER_NAME, PASSWORD) VALUES ('0e235279-ba2d-40f8-8cfa-c346be49eb47', 'Alvarez', 'Anne', 'Anne', 'password');
+INSERT INTO USER (USER_ID, LAST_NAME, FIRST_NAME, USER_NAME, PASSWORD) VALUES ('461f5182-9ab1-4e27-a1fb-0c2669bfbb55', 'Alvarez', 'Ann', 'Ann', 'password');
+INSERT INTO USER (USER_ID, LAST_NAME, FIRST_NAME, USER_NAME, PASSWORD) VALUES ('eeefd5a8-d4b8-49b0-af0d-a7e9eb170db6', 'Anderson', 'Betty', 'Betty', 'password');
+INSERT INTO USER (USER_ID, LAST_NAME, FIRST_NAME, USER_NAME, PASSWORD) VALUES ('5ee0a977-6856-455e-becb-0a4de6d08958', 'Anderson', 'Laura', 'Laura', 'password');
+INSERT INTO USER (USER_ID, LAST_NAME, FIRST_NAME, USER_NAME, PASSWORD) VALUES ('784234d6-5b90-4471-a51e-eb00940a5608', 'Armstrong', 'Christopher', 'Christopher', 'password');
